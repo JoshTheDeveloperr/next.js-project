d0d947466b942b74769e8c82a66b0e07109e2271
JoshTheNerd <joshuaharris1000@gmail.com>
2020-11-14 20:37:40 -0400

    fixed a bug

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,6 @@ export default function Home({ allPostsData  }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>…</section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         
         <h2 className={utilStyles.headingLg}>Blog</h2>
---
+++ posts/anotherBlog.md
@@ -0,0 +1,11 @@
+---
+title: 'Another blog'
+date: '2020-01-01'
+---
+
+**This is just a test**
+abc
+*def*
+***ghi**
+__jkl__
+```lmo```
